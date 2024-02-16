8804ffcbd09a008b4e5ebeb1647fb187afc905fd
indramhaarta <indramhrt@gmail.com>
2024-02-16 16:08:46 +0700

    fix: resolve error because of svg attribute

--- components/common/sidebar/components/upload-icon.tsx
+++ components/common/sidebar/components/upload-icon.tsx
@@ -7,9 +7,9 @@ export default function UploadIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-upload-cloud"
     >
       <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242"></path>
