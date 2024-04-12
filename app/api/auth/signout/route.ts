fb29fe125920e9f8bd5fdab3118e0a63bbdc54fd
indramhaarta <indramhrt@gmail.com>
2024-04-12 23:49:18 +0800

    fix: remove unused api

--- app/api/auth/signout/route.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  cookies().delete("accessToken");
-  return NextResponse.json(null, {
-    status: 200,
-  });
-}
